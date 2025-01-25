7242a7f224afd72d58d07a70dc728a4c84b8014e
ChrissDisigale <chrispro.genius@gmail.com>
2025-01-25 03:25:06 +0300

    implementing tb leprosy referral form

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/activity/CoreTbLeprosyProfileActivity.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/activity/CoreTbLeprosyProfileActivity.java
@@ -35,6 +35,7 @@ import org.smartregister.chw.core.utils.CoreJsonFormUtils;
 import org.smartregister.chw.core.utils.CoreReferralUtils;
 import org.smartregister.chw.core.utils.UpdateDetailsUtil;
 import org.smartregister.chw.tbleprosy.activity.BaseTbLeprosyProfileActivity;
+import org.smartregister.chw.tbleprosy.domain.MemberObject;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.domain.AlertStatus;
 import org.smartregister.family.util.JsonFormUtils;
@@ -240,13 +241,13 @@ public abstract class CoreTbLeprosyProfileActivity extends BaseTbLeprosyProfileA
 
         try {
             assert form != null;
-            startFormActivity(form);
+            startFormActivity(form, tbMemberObject);
         } catch (Exception e) {
             Timber.e(e);
         }
     }
 
-    public void startFormActivity(JSONObject jsonForm) {
+    public void startFormActivity(JSONObject jsonForm, MemberObject tbMemberObject) {
         Intent intent = org.smartregister.chw.core.utils.Utils.formActivityIntent(this, jsonForm.toString());
         startActivityForResult(intent, JsonFormUtils.REQUEST_CODE_GET_JSON);
     }
--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/contract/CoreTbLeprosyProfileContract.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/contract/CoreTbLeprosyProfileContract.java
@@ -2,10 +2,11 @@ package org.smartregister.chw.core.contract;
 
 import org.json.JSONObject;
 import org.smartregister.chw.tbleprosy.contract.TbLeprosyProfileContract;
+import org.smartregister.chw.tbleprosy.domain.MemberObject;
 
 public class CoreTbLeprosyProfileContract {
     public interface View extends TbLeprosyProfileContract.View {
-        void startFormActivity(JSONObject formJson);
+        void startFormActivity(JSONObject formJson, MemberObject tbMemberObject);
     }
 
     public interface Presenter extends TbLeprosyProfileContract.Presenter {
