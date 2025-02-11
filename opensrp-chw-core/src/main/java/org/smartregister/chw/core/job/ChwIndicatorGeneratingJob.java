f44c9133fb13f4fa1426fe7a41c3b5c362c4a52c
cozej4 <ilakozejumanne@gmail.com>
2025-02-11 12:42:33 +0300

    :hammer: Updated SyncServiceJob, ReportIndicatorGeneratingServiceJobs to make use of native android worker manager

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/job/ChwIndicatorGeneratingJob.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/job/ChwIndicatorGeneratingJob.java
@@ -4,6 +4,15 @@ import androidx.annotation.NonNull;
 
 import org.smartregister.reporting.job.RecurringIndicatorGeneratingJob;
 
+/**
+ * @deprecated As of release 1.6.3-MOH-SNAPSHOT, replaced by {@link org.smartregister.chw.core.worker.ReportIndicatorGeneratingWorker}.
+ * This class previously used the Evernote Job library, which is now deprecated.
+ * The new implementation leverages Android's native {@link androidx.work.WorkManager} for better
+ * compatibility, reliability, and efficiency in background task scheduling.
+ *
+ * Please migrate to {@link org.smartregister.chw.core.worker.ReportIndicatorGeneratingWorker} to ensure future compatibility.
+ */
+@Deprecated
 public class ChwIndicatorGeneratingJob extends RecurringIndicatorGeneratingJob {
     @NonNull
     protected Result onRunJob(@NonNull Params params) {
