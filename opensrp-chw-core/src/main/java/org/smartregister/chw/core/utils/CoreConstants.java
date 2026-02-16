6e3eab4a14b10b499e8cd9ece24a406aa1d61267
cozej4 <ilakozejumanne@gmail.com>
2026-02-16 18:00:10 +0300

    🛠️ Add harm reduction sober house constants

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/utils/CoreConstants.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/utils/CoreConstants.java
@@ -1012,6 +1012,7 @@ public class CoreConstants {
         public static final String VMMC = "VMMC";
         public static final String TBLEPROSY = "TB/Leprosy";
         public static final String HARM_REDUCTION = "Harm Reduction";
+        public static final String HARM_REDUCTION_SOBER_HOUSE = "Harm Reduction Sober House";
         public static final String PMTCT_FOLLOWUP = "PMTCT Followup";
         public static final String MOTHER_CHAMPION = "Mother Champion";
         public static final String CBHS_CLIENTS = "CBHS Clients";
@@ -1302,6 +1303,7 @@ public class CoreConstants {
         public static final String VMMC_REGISTER_ACTIVITY = "VMMC_REGISTER_ACTIVITY";
         public static final String TBLEPROSY_REGISTER_ACTIVITY = "TBLEPROSY_REGISTER_ACTIVITY";
         public static final String HARM_REDUCTION_REGISTER_ACTIVITY = "HARM_REDUCTION_REGISTER_ACTIVITY";
+        public static final String HARM_REDUCTION_SOBER_HOUSE_REGISTER_ACTIVITY = "HARM_REDUCTION_SOBER_HOUSE_REGISTER_ACTIVITY";
         public static final String PMTCT_FOLLOWUP_REGISTER_ACTIVITY = "PMTCT_FOLLOWUP_REGISTER_ACTIVITY";
         public static final String HEI = "HEI_REGISTER_ACTIVITY";
         public static final String MOTHER_CHAMPION_ACTIVITY = "MOTHER_CHAMPION_ACTIVITY";
