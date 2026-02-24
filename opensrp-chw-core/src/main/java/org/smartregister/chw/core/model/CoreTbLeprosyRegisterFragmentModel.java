6e2a725924de9f5e874e7abae1604263ec4ea4da
cozej4 <ilakozejumanne@gmail.com>
2026-02-24 14:23:27 +0300

    🐛 Include TB client number in TB/Leprosy register query

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/model/CoreTbLeprosyRegisterFragmentModel.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/model/CoreTbLeprosyRegisterFragmentModel.java
@@ -33,6 +33,7 @@ public class CoreTbLeprosyRegisterFragmentModel extends BaseTbLeprosyRegisterFra
         Set<String> columnList = new HashSet<>();
 
         columnList.add(tableName + "." + DBConstants.KEY.BASE_ENTITY_ID);
+        columnList.add(tableName + "." + org.smartregister.chw.tbleprosy.util.DBConstants.KEY.TB_CLIENT_NUMBER);
         columnList.add(CoreConstants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.RELATIONAL_ID + " as " + ChildDBConstants.KEY.RELATIONAL_ID);
         columnList.add(CoreConstants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.FIRST_NAME);
         columnList.add(CoreConstants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.MIDDLE_NAME);
